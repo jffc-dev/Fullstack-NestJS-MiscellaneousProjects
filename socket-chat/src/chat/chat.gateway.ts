3372a87e6c0355e8f7c329b8fc5492d3d683a073
Javier Flores <jffc.dev@gmail.com>
2023-11-26 15:21:58 -0500

    feat: initial socket added

---
+++ socket-chat/src/chat/chat.gateway.ts
@@ -0,0 +1,22 @@
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ChatService } from './chat.service';
+import { OnModuleInit } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway implements OnModuleInit {
+  @WebSocketServer()
+  public server: Server;
+
+  constructor(private readonly chatService: ChatService) {}
+
+  onModuleInit() {
+    this.server.on('connection', (socket: Socket) => {
+      console.log('client connected: ', socket.id);
+
+      socket.on('disconnect', () => {
+        console.log('client disconnected:', socket.id);
+      });
+    });
+  }
+}
