a6fde7001a90f8c7045b3e841295a2baff94e468
Javier Flores <jffc.dev@gmail.com>
2023-11-27 23:37:49 -0500

    feat: label states added

--- socket-chat/public/js/chat.js
+++ socket-chat/public/js/chat.js
@@ -1,5 +1,10 @@
 const username = localStorage.getItem('name');
 
+//HTML References
+
+const labelStatusOn = document.querySelector('#status-online');
+const labelStatusOff = document.querySelector('#status-offline');
+
 if (!username) {
   window.location.replace('/');
   throw new Error('Username is required');
@@ -10,4 +15,16 @@ const socket = io('http://localhost:3002', {
     token: 'abc-123',
     name: username,
   },
+});
+
+socket.on('connect', () => {
+  console.log('Connected');
+  labelStatusOn.classList.remove('hidden');
+  labelStatusOff.classList.add('hidden');
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected');
+  labelStatusOff.classList.remove('hidden');
+  labelStatusOn.classList.add('hidden');
 });
